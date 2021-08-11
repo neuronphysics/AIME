1de7d9774c155c9e682880e55cd10104b17c85c8
ericd2017 <luodongshu1998@gmail.com>
2021-08-10 23:56:36 -0700

    preprocess data

--- aime/data/data_loader.py
+++ aime/data/data_loader.py
@@ -1,9 +1,10 @@
 import gym
 from envs import register_envs
+import torch
 
 register_envs()
 
-def load_data(env_name, mode=None, num_random_episodes=1000, num_lagging_observation=2, num_lagging_action=1):
+def generate_data(env_name, mode=None, num_random_episodes=1000, num_lagging_observation=2, num_lagging_action=1):
     env = gym.make(env_name)
     episode_list = []
     num_current_episodes = 0
@@ -38,11 +39,27 @@ def load_data(env_name, mode=None, num_random_episodes=1000, num_lagging_observa
             num_current_episodes += 1
     # save episode data to pickle file
     # episode_list.to_pickle()
+    env.close()
     return episode_list
 
+def preprocess_data(data):
+    observation_tensor = torch.stack([torch.Tensor(d["observation_list"]) for d in data])
+    image_tensor = torch.stack([torch.Tensor(d["image_list"]) for d in data])
+    action_tensor = torch.stack([torch.Tensor(d["action_list"]) for d in data])
+    reward_tensor = torch.stack([torch.Tensor(d["reward_list"]) for d in data])
+    preprocessed_data = {
+        "observation_tensor": observation_tensor,
+        "image_tensor": image_tensor,
+        "action_tensor": action_tensor,
+        "reward_tensor": reward_tensor
+    }
+    return preprocessed_data
+    
+def load_data(env_name, mode=None, num_random_episodes=1000, num_lagging_observation=2, num_lagging_action=1):
+    data = generate_data(env_name, mode, num_random_episodes, num_lagging_observation, num_lagging_action)
+    return preprocess_data(data)
+
 if __name__ == '__main__':
-    episode_list = load_data('MountainCarContinuous-v0', num_random_episodes=10)
+    episode_list = load_data('MountainCarContinuous-v0', num_random_episodes=1)
     print(episode_list)
-    
-    
-            
+
