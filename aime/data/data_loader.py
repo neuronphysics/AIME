9ccd5bf75440d0c72a59ddfb134ac8ed82b866e5
ericd2017 <luodongshu1998@gmail.com>
2021-08-11 00:52:01 -0700

    use observation as image

--- aime/data/data_loader.py
+++ aime/data/data_loader.py
@@ -11,29 +11,23 @@ def generate_data(env_name, mode=None, num_random_episodes=1000, num_lagging_obs
     while num_current_episodes < num_random_episodes:
         obs_list = []
         reward_list = []
-        image_list = []
         action_list = []
         obs = env.reset()
-        image = env.render(mode="rgb_array")
         obs_list.append(obs)
-        image_list.append(image)
         done = False
         num_steps = 0
         while not done:
             action = env.action_space.sample()
             obs, reward, done, info = env.step(action)
-            image = env.render(mode="rgb_array")
             action_list.append(action)
             obs_list.append(obs)
             reward_list.append(reward)
-            image_list.append(image)
             num_steps += 1
         if (num_steps >= num_lagging_action) and (num_steps+1 >= num_lagging_observation):
             print(num_steps)
             episode_data = {
                 "observation_list": obs_list,
                 "reward_list": reward_list,
-                "image_list": image_list,
                 "action_list": action_list
             }
             episode_list.append(episode_data)
@@ -45,12 +39,10 @@ def generate_data(env_name, mode=None, num_random_episodes=1000, num_lagging_obs
 
 def preprocess_data(data):
     observation_tensor = torch.Tensor([d["observation_list"] for d in data])
-    image_tensor = torch.Tensor([d["image_list"] for d in data])
     action_tensor = torch.Tensor([d["action_list"] for d in data])
     reward_tensor = torch.Tensor([d["reward_list"] for d in data])
     preprocessed_data = {
         "observation_tensor": observation_tensor,
-        "image_tensor": image_tensor,
         "action_tensor": action_tensor,
         "reward_tensor": reward_tensor
     }
@@ -61,6 +53,6 @@ def load_data(env_name, mode=None, num_random_episodes=1000, num_lagging_observa
     return preprocess_data(data)
 
 if __name__ == '__main__':
-    episode_list = load_data('MountainCarContinuous-v0', num_random_episodes=4)
+    episode_list = load_data('CarRacing-v0', num_random_episodes=4)
     print(episode_list)
 
