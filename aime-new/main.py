fix kl

aime-new/main.py
@@ -243,18 +243,19 @@ for episode in tqdm(range(metrics['episodes'][-1] + 1, args.episodes + 1), total
       outputs = actor_critic_planner.transition_gp.predict(
           scaled_X, suppress_eval_mode_warning=False
       )
-      episode_state_kl = -torch.sum(torch.stack(actor_critic_planner.transition_gp.get_mll(outputs, episode_states[1:])), dim=-1, keepdim=True)
+      episode_state_kl = torch.square(episode_states[1:] - outputs)
+      state_transition_loss = -torch.sum(torch.stack(actor_critic_planner.transition_gp.get_mll(outputs, episode_states[1:])))
   ##
-  soft_v_values = episode_q_values[:-1] - episode_state_kl[:-1] - episode_policy_kl[1:]
-  target_q_values = args.temperature_factor * episode_rewards[:-1] + args.discount_factor * episode_values[1:]
-  value_loss = F.mse_loss(episode_values[1:], soft_v_values, reduction='none').mean()
-  q_loss = F.mse_loss(episode_q_values[:-1], target_q_values, reduction='none').mean()
+  soft_v_values = episode_q_values - episode_state_kl - episode_policy_kl
+  target_q_values = args.temperature_factor * episode_rewards + args.discount_factor * episode_values
+  value_loss = F.mse_loss(episode_values, soft_v_values, reduction='none').mean()
+  q_loss = F.mse_loss(episode_q_values, target_q_values, reduction='none').mean()
   policy_loss = (episode_policy_kl - episode_q_values + episode_values).mean()
   kl_transition_loss = (torch.exp(episode_rewards + episode_values - episode_q_values) * episode_state_kl).mean()
   
   planning_optimiser.zero_grad()
   # may add an action entropy
-  (value_loss + q_loss + policy_loss + kl_transition_loss + torch.sum(episode_state_kl)).backward(retain_graph=True)
+  (value_loss + q_loss + policy_loss + kl_transition_loss + state_transition_loss).backward(retain_graph=True)
   nn.utils.clip_grad_norm_(actor_critic_planner.parameters(), args.grad_clip_norm, norm_type=2)
   planning_optimiser.step()
   