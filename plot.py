6413a98ddf7e5aba6e390097c207b6616fc8f89e
ericd2017 <luodongshu1998@gmail.com>
2022-05-15 01:43:59 -0700

    add SLAC to plot code

--- plot.py
+++ plot.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 import torch
+import pandas as pd
 
 import seaborn as sns
 
@@ -36,14 +37,26 @@ def planet_results(file_path):
     test_rewards = np.squeeze(np.array(data['test_rewards']))
     return (steps, test_rewards)
 
-env = "loop"
+def slac_results(file_path):
+    data = pd.read_csv(file_path)
+    steps = data['step']
+    returns = data['return']
+    return (steps, returns)
 
-dreamerv2_steps_seed_1, dreamerv2_eval_returns_seed1 = dreamerv2_results(f"dreamerv2/{env}/1/metrics_1.jsonl")
-dreamerv2_steps_seed_2, dreamerv2_eval_returns_seed2 = dreamerv2_results(f"dreamerv2/{env}/2/metrics_2.jsonl")
-dreamerv2_steps_seed_3, dreamerv2_eval_returns_seed3 = dreamerv2_results(f"dreamerv2/{env}/3/metrics_3.jsonl")
+env = "walker_walk"
+dreamerv2_steps_seed_1, dreamerv2_eval_returns_seed1 = dreamerv2_results(f"dreamerv2/dreamerv2_dmc/{env}/run0.jsonl")
+dreamerv2_steps_seed_2, dreamerv2_eval_returns_seed2 = dreamerv2_results(f"dreamerv2/dreamerv2_dmc/{env}/run1.jsonl")
+dreamerv2_steps_seed_3, dreamerv2_eval_returns_seed3 = dreamerv2_results(f"dreamerv2/dreamerv2_dmc/{env}/run2.jsonl")
 num_dreamerv2samples = min(len(dreamerv2_steps_seed_1), len(dreamerv2_steps_seed_2), len(dreamerv2_steps_seed_3))
 dreamerv2_average_eval_return = (dreamerv2_eval_returns_seed1[:num_dreamerv2samples] + dreamerv2_eval_returns_seed2[:num_dreamerv2samples] + dreamerv2_eval_returns_seed3[:num_dreamerv2samples])/3
 dreamerv2_average_steps = (dreamerv2_steps_seed_1[:num_dreamerv2samples] + dreamerv2_steps_seed_2[:num_dreamerv2samples] + dreamerv2_steps_seed_3[:num_dreamerv2samples])/3
+
+slac_steps_seed_1, slac_returns_seed_1 = slac_results(f"slac/{env}/1/log.csv")
+slac_steps_seed_2, slac_returns_seed_2 = slac_results(f"slac/{env}/2/log.csv")
+slac_steps_seed_3, slac_returns_seed_3 = slac_results(f"slac/{env}/3/log.csv")
+num_slac_samples = min(len(slac_steps_seed_1), len(slac_steps_seed_2), len(slac_steps_seed_3))
+slac_average_return = (slac_returns_seed_1[:num_slac_samples] + slac_returns_seed_2[:num_slac_samples] + slac_returns_seed_3[:num_slac_samples]) / 3
+slac_average_steps = (slac_steps_seed_1[:num_slac_samples] + slac_steps_seed_2[:num_slac_samples] + slac_steps_seed_3[:num_slac_samples]) / 3
 
 '''
 planet_steps_seed1, planet_test_rewards_seed1 = planet_results(f"planet/{env}/1/metrics.pth")
@@ -51,7 +64,6 @@ planet_steps_seed2, planet_test_rewards_seed2 = planet_results(f"planet/{env}/2/
 planet_steps_seed3, planet_test_rewards_seed3 = planet_results(f"planet/{env}/3/metrics.pth")
 num_planet_samples = min(len(planet_steps_seed1), len(planet_steps_seed2), len(planet_steps_seed3))
 planet_average_eval_return = (planet_test_rewards_seed1[:num_planet_samples] + planet_test_rewards_seed2[:num_planet_samples] + planet_test_rewards_seed3[:num_planet_samples])/3
-'''
 regular_vae_steps_seed1, regular_vae_test_rewards_seed1 = new_model_results(f"regular_vae/{env}/1/metrics.pth")
 regular_vae_steps_seed2, regular_vae_test_rewards_seed2 = new_model_results(f"regular_vae/{env}/2/metrics.pth")
 regular_vae_steps_seed3, regular_vae_test_rewards_seed3 = new_model_results(f"regular_vae/{env}/3/metrics.pth")
@@ -65,11 +77,12 @@ infinite_vae_steps_seed3, infinite_vae_test_rewards_seed3 = new_model_results(f"
 num_infinite_vae_samples = min(len(infinite_vae_steps_seed1), len(infinite_vae_steps_seed2), len(infinite_vae_steps_seed3))
 infinite_vae_average_eval_return = (infinite_vae_test_rewards_seed1[:num_infinite_vae_samples] + infinite_vae_test_rewards_seed2[:num_infinite_vae_samples] + infinite_vae_test_rewards_seed3[:num_infinite_vae_samples])/3
 infinite_vae_average_steps = (infinite_vae_steps_seed1[:num_infinite_vae_samples] + infinite_vae_steps_seed2[:num_infinite_vae_samples] + infinite_vae_steps_seed3[:num_infinite_vae_samples])/3
-
+'''
 plt.plot(dreamerv2_average_steps, dreamerv2_average_eval_return, label="Dreamerv2")
+plt.plot(slac_average_steps, slac_average_return, label="Slac")
 #plt.plot(planet_steps_seed1[:num_planet_samples], planet_average_eval_return, label="Planet")
-plt.plot(regular_vae_average_steps, regular_vae_average_eval_return, label="D2E (Regular VAE)")
-plt.plot(infinite_vae_average_steps, infinite_vae_average_eval_return, label="D2E (Infinite Mixture VAE)")
+#plt.plot(regular_vae_average_steps, regular_vae_average_eval_return, label="D2E (Regular VAE)")
+#plt.plot(infinite_vae_average_steps, infinite_vae_average_eval_return, label="D2E (Infinite Mixture VAE)")
 
 plt.legend()
 plt.title(f"{env.replace('_', ' ').capitalize()}")
