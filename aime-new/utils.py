22b751e1cce436cc4b8a7d78c7e27dea47ecc9df
Eric Luo <30668005+2017soft@users.noreply.github.com>
2022-05-15 01:34:31 -0700

    Add DeepMind Control Suite to D2E and Some Other Fix (#14)
    * Added infinite Variational Autoencoder model
    
    * Added infinite Gaussiian Mixture Variational Autoencoder model with adding w term as prior
    
    * Added an incomplete function for sampling W variable
    
    * Add files via upload
    
    * update new version of infinte GMM VAE model
    
    * changes in infinite GMM VAE
    
    * rename module and file
    
    * try using AdaBound as the optimizer
    
    * change default hyperparameters
    
    * reduce latent size and hidden size for memory issues
    
    * add dm_control and remove smarts
    
    * make videos for original image only
    
    * add 3 more environments
    
    Co-authored-by: neuronphysics <sheikhbahaee@gmail.com>

--- aime-new/utils.py
+++ aime-new/utils.py
@@ -1,5 +1,5 @@
 import os
-import cv2
+#import cv2
 import numpy as np
 import plotly
 from plotly.graph_objs import Scatter
@@ -31,9 +31,12 @@ def lineplot(xs, ys_population, title, path='', xaxis='episode'):
 
 
 def write_video(frames, title, path=''):
+  '''
   frames = np.multiply(np.stack(frames, axis=0).transpose(0, 2, 3, 1), 255).clip(0, 255).astype(np.uint8)[:, :, :, ::-1]  # VideoWrite expects H x W x C in BGR
   _, H, W, _ = frames.shape
   writer = cv2.VideoWriter(os.path.join(path, '%s.mp4' % title), cv2.VideoWriter_fourcc(*'mp4v'), 30., (W, H), True)
   for frame in frames:
     writer.write(frame)
   writer.release()
+  '''
+  pass
