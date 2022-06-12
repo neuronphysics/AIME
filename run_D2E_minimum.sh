3e3e3ee08b114c3400b71fd46d1daf38e479e20e
Zahra Sheikhbahaee <memole@beluga2.int.ets1.calculquebec.ca>
2022-06-11 20:13:12 -0400

    fix a bug and update slurm script

--- run_D2E_minimum.sh
+++ run_D2E_minimum.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+#
+#SBATCH --nodes=2
+#SBATCH --gres=gpu:v100:2
+#SBATCH --tasks-per-node=2
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=30G
 #SBATCH --time=00-23:59
 #SBATCH --account=def-jhoey
-#SBATCH --ntasks=1
-#SBATCH --mem=16G
-#SBATCH --cpus-per-task=2
-#SBATCH --gres=gpu:1
 #SBATCH --output=../cheetah-run-seed-1_%N-%j.out
 
+module load StdEnv/2020
 module load python/3.8.10
-
+module load scipy-stack
 source /home/memole/dm_control/bin/activate
-python main.py --id cheetah-run-seed-1 --seed 1 --env cheetah-run --use-regular-vae
+python main.py --id cheetah-run-seed-1 --seed 1 --env cheetah-run --use-regular-vae  
