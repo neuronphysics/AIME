b02a36207bf4c61b11ab7e4092b025449a39746d
Pengyuan Shi <p23shi@gra-login1.graham.sharcnet>
2022-06-23 05:32:03 -0400

    add transition and controller loss

---
+++ sbatch_scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=00-2:59
+#SBATCH --account=def-jhoey
+#SBATCH --ntasks=1
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=3
+#SBATCH --gres=gpu:1
+#SBATCH --output=cheetah-run-seed-2.out
+
+module load python/3.8.10
+cd AIME
+source ~/dm_control/bin/activate
+python main.py --id cheetah-run-seed-1 --seed 1 --env cheetah-run --use-regular-vae --embedding-size 512 --input-size 32 --max-episode-length 200
