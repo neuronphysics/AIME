78ca0f6c65c642b6e1f038a2a61251bfa08c14b9
Zahra Sheikhbahaee <memole@beluga1.int.ets1.calculquebec.ca>
2022-07-30 14:15:34 -0400

    add an experiment t test the code

---
+++ run_EIM_debug_minimum.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --nodes=2
+#SBATCH --gres=gpu:v100:2
+#SBATCH --tasks-per-node=2
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=30G
+#SBATCH --time=00-23:59
+#SBATCH --account=def-jhoey
+#SBATCH --output=../cheetah-run-seed-1_%N-%j.out
+#SBATCH --error=../cheetah-run-seed-1_%N-%j.err
+module load StdEnv/2020
+module load python/3.8.10
+module load scipy-stack
+source /home/memole/dm_control/bin/activate
+pip install --no-index --upgrade pip
+
+pip install --no-index absl-py
+python main.py --id cheetah-run-seed-1 --seed 1 --env cheetah-run --use-regular-vae  
