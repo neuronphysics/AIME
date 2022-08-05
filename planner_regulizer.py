e37071ffed4ffd5ac4b407bbdf2ec2f4e6a8b5b2
Zahra Sheikhbahaee <memole@beluga2.int.ets1.calculquebec.ca>
2022-08-05 15:10:09 -0400

    added some codes which should be fixed (convert to pytorch)

--- planner_regulizer.py
+++ planner_regulizer.py
@@ -883,4 +883,193 @@ class D2EAgent(Agent):
         print("load checkpoint from \"" + self.checkpoint_path +
               "\" at " + str(self._global_step) + " time step")
 
+##########################
+#work in progress
+#Needs to convert to a pytorch code
+#shorturl.at/oprZ1
+def eval_policy_episodes(env, policy, n_episodes):
+  """Evaluates policy performance."""
+  results = []
+  for _ in range(n_episodes):
+    time_step = env.reset()
+    total_rewards = 0.0
+    while not time_step.is_last().numpy()[0]:
+      action = policy(time_step.observation)[0]
+      time_step = env.step(action)
+      total_rewards += time_step.reward
+    results.append(total_rewards)
+  results = np.array(results)
+  return float(np.mean(results)), float(np.std(results))
 
+
+def eval_policies(env, policies, n_episodes):
+  results_episode_return = []
+  infos = collections.OrderedDict()
+  for name, policy in policies.items():
+    mean, _ = eval_policy_episodes(env, policy, n_episodes)
+    results_episode_return.append(mean)
+    infos[name] = collections.OrderedDict()
+    infos[name]['episode_mean'] = mean
+  results = results_episode_return
+  return results, infos
+
+
+# TODO(wuyifan): external version for loading environments
+def env_factory(env_name):
+  py_env = suite_mujoco.load(env_name)
+  tf_env = tf_py_environment.TFPyEnvironment(py_env)
+  return tf_env
+
+
+def get_transition(time_step, next_time_step, action, next_action):
+  return dataset.Transition(
+      s1=time_step.observation,
+      s2=next_time_step.observation,
+      a1=action,
+      a2=next_action,
+      reward=next_time_step.reward,
+      discount=next_time_step.discount)
+
+
+class DataCollector(object):
+  """Class for collecting sequence of environment experience."""
+
+  def __init__(self, env, policy, data):
+    self._env = env
+    self._policy = policy
+    self._data = data
+    self._saved_action = None
+
+  def collect_transition(self):
+    """Collect single transition from environment."""
+    time_step = self._env.current_time_step()
+    if self._saved_action is None:
+      self._saved_action = self._policy(time_step.observation)[0]
+    action = self._saved_action
+    next_time_step = self._env.step(action)
+    next_action = self._policy(next_time_step.observation)[0]
+    self._saved_action = next_action
+    if not time_step.is_last()[0].numpy():
+      transition = get_transition(time_step, next_time_step,
+                                  action, next_action)
+      self._data.add_transitions(transition)
+      return 1
+    else:
+      return 0
+
+Transition = collections.namedtuple(
+    'Transition', 's1, s2, a1, a2, discount, reward')
+
+
+class DatasetView(object):
+  """Interface for reading from dataset."""
+
+  def __init__(self, dataset, indices):
+    self._dataset = dataset
+    self._indices = indices
+
+  def get_batch(self, indices):
+    real_indices = self._indices[indices]
+    return self._dataset.get_batch(real_indices)
+
+  @property
+  def size(self):
+    return self._indices.shape[0]
+
+
+def save_copy(data, ckpt_name):
+  """Creates a copy of the current data and save as a checkpoint."""
+  new_data = Dataset(
+      observation_spec=data.config['observation_spec'],
+      action_spec=data.config['action_spec'],
+      size=data.size,
+      circular=False)
+  full_batch = data.get_batch(np.arange(data.size))
+  new_data.add_transitions(full_batch)
+  data_ckpt = tf.train.Checkpoint(data=new_data)
+  data_ckpt.write(ckpt_name)
+
+
+class Dataset(nn.Module):
+  """Tensorflow module of dataset of transitions."""
+
+  def __init__(
+      self,
+      observation_spec,
+      action_spec,
+      size,
+      circular=True,
+      ):
+    super(Dataset, self).__init__()
+    self._size = size
+    self._circular = circular
+    obs_shape = list(observation_spec.shape)
+    obs_type = observation_spec.dtype
+    action_shape = list(action_spec.shape)
+    action_type = action_spec.dtype
+    self._s1 = self._zeros([size] + obs_shape, obs_type)
+    self._s2 = self._zeros([size] + obs_shape, obs_type)
+    self._a1 = self._zeros([size] + action_shape, action_type)
+    self._a2 = self._zeros([size] + action_shape, action_type)
+    self._discount = self._zeros([size], tf.float32)
+    self._reward = self._zeros([size], tf.float32)
+    self._data = Transition(
+        s1=self._s1, s2=self._s2, a1=self._a1, a2=self._a2,
+        discount=self._discount, reward=self._reward)
+    self._current_size = tf.Variable(0)
+    self._current_idx = tf.Variable(0)
+    self._capacity = tf.Variable(self._size)
+    self._config = collections.OrderedDict(
+        observation_spec=observation_spec,
+        action_spec=action_spec,
+        size=size,
+        circular=circular)
+
+  @property
+  def config(self):
+    return self._config
+
+  def create_view(self, indices):
+    return DatasetView(self, indices)
+
+  def get_batch(self, indices):
+    indices = tf.constant(indices)
+    def get_batch_(data_):
+      return tf.gather(data_, indices)
+    transition_batch = tf.nest.map_structure(get_batch_, self._data)
+    return transition_batch
+
+  @property
+  def data(self):
+    return self._data
+
+  @property
+  def capacity(self):
+    return self._size
+
+  @property
+  def size(self):
+    return self._current_size.numpy()
+
+  def _zeros(self, shape, dtype):
+    """Create a variable initialized with zeros."""
+    return tf.Variable(tf.zeros(shape, dtype))
+
+  @tf.function
+  def add_transitions(self, transitions):
+    assert isinstance(transitions, Transition)
+    batch_size = transitions.s1.shape[0]
+    effective_batch_size = tf.minimum(
+        batch_size, self._size - self._current_idx)
+    indices = self._current_idx + tf.range(effective_batch_size)
+    for key in transitions._asdict().keys():
+      data = getattr(self._data, key)
+      batch = getattr(transitions, key)
+      tf.scatter_update(data, indices, batch[:effective_batch_size])
+    # Update size and index.
+    if tf.less(self._current_size, self._size):
+      self._current_size.assign_add(effective_batch_size)
+    self._current_idx.assign_add(effective_batch_size)
+    if self._circular:
+      if tf.greater_equal(self._current_idx, self._size):
+        self._current_idx.assign(0)
