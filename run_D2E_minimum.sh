bb23f11514c4263cb7580ce9a06579222a347ffa
Zahra Sheikhbahaee <memole@beluga4.int.ets1.calculquebec.ca>
2022-09-14 23:09:36 -0400

    running code

--- run_D2E_minimum.sh
+++ run_D2E_minimum.sh
@@ -5,13 +5,20 @@
 #SBATCH --tasks-per-node=2
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=30G
-#SBATCH --time=00-23:59
+#SBATCH --time=00-01:59
 #SBATCH --account=def-jhoey
-#SBATCH --output=../cheetah-run-seed-1_%N-%j.out
-#SBATCH --error=../cheetah-run-seed-1_%N-%j.err
+#SBATCH --output=/home/memole/TEST/HalfCheetah-run-seed-1_%N-%j.out
+#SBATCH --error=/home/memole/TEST/HalfCheetah-run-seed-1_%N-%j.err
 module load StdEnv/2020
 module load python/3.8.10
 module load scipy-stack
 source /home/memole/dm_control/bin/activate
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/memole/.mujoco/mujoco210/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
+pip install --upgrade pip
+pip install dill --no-index
 
-python main.py --id cheetah-run-seed-1 --seed 1 --env cheetah-run --use-regular-vae  
+#python DataCollectionD2E.py --env_name=HalfCheetah-v2
+python train_eval_offline_D2E.py --env_name=HalfCheetah-v2
+
+#python main.py --id cheetah-run-seed-1 --seed 1 --env cheetah-run --use-regular-vae  
