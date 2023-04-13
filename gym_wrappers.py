b6623bb4a50be88bb0b27e8e8b572f3ce94a4bcf
Zahra Sheikhbahaee <memole@beluga4.int.ets1.calculquebec.ca>
2023-04-13 10:09:32 -0400

    add more module from https://github.com/HorizonRobotics/alf

--- gym_wrappers.py
+++ gym_wrappers.py
@@ -22,10 +22,10 @@ import cv2
 import gym
 import numpy as np
 import random
-
-
+from alf_environment import TimeLimit
+from alf_gym_wrapper import AlfGymWrapper
 from nest import transform_nest
-
+import gym.spaces
 
 def transform_space(observation_space, field, func):
     """Transform the child space in observation_space indicated by field using func
@@ -733,3 +733,85 @@ class NonEpisodicEnv(gym.Wrapper):
     def step(self, action):
         ob, reward, done, info = self.env.step(action)
         return ob, reward, False, info
+
+
+def wrap_env(gym_env,
+             env_id=None,
+             discount=1.0,
+             max_episode_steps=0,
+             gym_env_wrappers=(),
+             time_limit_wrapper=TimeLimit,
+             normalize_action=True,
+             clip_action=True,
+             alf_env_wrappers=(),
+             image_channel_first=True,
+             auto_reset=True):
+    """Wraps given gym environment with AlfGymWrapper.
+
+    Note that by default a TimeLimit wrapper is used to limit episode lengths
+    to the default benchmarks defined by the registered environments.
+
+    Also note that all gym wrappers assume images are 'channel_last' by default,
+    while PyTorch only supports 'channel_first' image inputs. To enable this
+    transpose, 'image_channel_first' is set as True by default. ``gym_wrappers.ImageChannelFirst``
+    is applied after all gym_env_wrappers and before the AlfGymWrapper.
+
+    Args:
+        gym_env (gym.Env): An instance of OpenAI gym environment.
+        env_id (int): (optional) ID of the environment.
+        discount (float): Discount to use for the environment.
+        max_episode_steps (int): Used to create a TimeLimitWrapper. No limit is applied
+            if set to 0. Usually set to `gym_spec.max_episode_steps` as done in `load.
+            Note that a ``TimeLimit`` wrapper will be applied as the *last* Gym wrapper,
+            so if you also use the ``FrameSkip`` Gym wrapper, then the actual max length
+            of an episode will be ``skip*max_episode_steps``.
+        gym_env_wrappers (Iterable): Iterable with references to gym_wrappers,
+            classes to use directly on the gym environment.
+        time_limit_wrapper (AlfEnvironmentBaseWrapper): Wrapper that accepts
+            (env, max_episode_steps) params to enforce a TimeLimit. Usually this
+            should be left as the default, alf_wrappers.TimeLimit.
+        normalize_action (bool): if True, will scale continuous actions to
+            ``[-1, 1]`` to be better used by algorithms that compute entropies.
+        clip_action (bool): If True, will clip continuous action to its bound specified
+            by ``action_spec``. If ``normalize_action`` is also ``True``, this
+            clipping happens after the normalization (i.e., clips to ``[-1, 1]``).
+        alf_env_wrappers (Iterable): Iterable with references to alf_wrappers
+            classes to use on the ALF environment.
+        image_channel_first (bool): whether transpose image channels to first dimension.
+            PyTorch only supports channgel_first image inputs.
+        auto_reset (bool): If True (default), reset the environment automatically after a
+            terminal state is reached.
+
+    Returns:
+        An AlfEnvironment instance.
+    """
+
+    for wrapper in gym_env_wrappers:
+        gym_env = wrapper(gym_env)
+
+    # To apply channel_first transpose on gym (py) env
+    if image_channel_first:
+        gym_env = ImageChannelFirst(gym_env)
+
+    if normalize_action:
+        # normalize continuous actions to [-1, 1]
+        gym_env = NormalizedAction(gym_env)
+
+    if clip_action:
+        # clip continuous actions according to gym_env.action_space
+        gym_env = ContinuousActionClip(gym_env)
+
+    env = AlfGymWrapper(
+        gym_env=gym_env,
+        env_id=env_id,
+        discount=discount,
+        auto_reset=auto_reset,
+    )
+
+    if max_episode_steps > 0:
+        env = time_limit_wrapper(env, max_episode_steps)
+
+    for wrapper in alf_env_wrappers:
+        env = wrapper(env)
+
+    return env
