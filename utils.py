06f1dcc23d7def8712a7756e7cd9eb3cfb71be01
Zahra Sheikhbahaee <memole@beluga2.int.ets1.calculquebec.ca>
2023-05-19 18:54:36 -0400

    fix LayerNorm2d

--- utils.py
+++ utils.py
@@ -205,34 +205,16 @@ class AttentionBlock(nn.Module):
         return score
 
 class LayerNorm2d(nn.LayerNorm):
-    
-    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first. 
-    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
-    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
-    with shape (batch_size, channels, height, width).
-    source :https://github.com/facebookresearch/ConvNeXt/blob/main/models/convnext.py
-    """
-    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_first", **kwargs):
-        super().__init__(normalized_shape, **kwargs)
-        self.weight = nn.Parameter(torch.ones(normalized_shape))
-        self.bias = nn.Parameter(torch.zeros(normalized_shape))
-        self.eps = eps
-        self.data_format = data_format
-        if self.data_format not in ["channels_last", "channels_first"]:
-            raise NotImplementedError 
-        self.normalized_shape = (normalized_shape, )
-    
-    def forward(self, x):
-        assert x.dim() == 4, 'LayerNorm2d only supports inputs with shape ' \
-            f'(B, C, H, W), but got tensor with shape {x.shape}'
-        if self.data_format == "channels_last":
-            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
-        elif self.data_format == "channels_first":
-            u = x.mean(1, keepdim=True)
-            s = (x - u).pow(2).mean(1, keepdim=True)
-            x = (x - u) / torch.sqrt(s + self.eps)
-            x = self.weight[:, None, None] * x + self.bias[:, None, None]
-            return x
+    """LayerNorm for channels of '2D' spatial NCHW tensors"""
+
+    def __init__(self, num_channels, eps=1e-6, affine=True):
+        super().__init__(num_channels, eps=eps, elementwise_affine=affine)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return F.layer_norm(
+            x.permute(0, 2, 3, 1), self.normalized_shape, self.weight, self.bias, self.eps
+        ).permute(0, 3, 1, 2)
+            
 
 class ResidualBlock(nn.Module):
 
