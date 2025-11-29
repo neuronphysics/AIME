2b365ab20906106506361eacbf4dfb72529b3c59
Zahra Sheikhbahaee <memole@login1.int.fir.alliancecan.ca>
2025-11-28 19:12:13 -0800

    change the requirements.txt script

--- run_VRNN_minimum.sh
+++ run_VRNN_minimum.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=DPGMM
 #SBATCH --nodes=1
-#SBATCH --gpus=h100_80gb:1 
-#SBATCH --ntasks-per-node=2
+#SBATCH --gpus=h100:1 
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=100G
 #SBATCH --time=01-20:59
@@ -13,21 +13,29 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
-module load gcc/12.3 python/3.11 mujoco/3.3.0 cuda/12.2
-module load StdEnv/2023  
+#Start clean
+module load StdEnv/2023
+module load gcc/12.3
+
+module load cuda/12.2
+
+module load python/3.11
+module load scipy-stack/2024a   # required by opencv/4.10.0
+
+module load opencv/4.10.0
+
+module load mujoco/3.3.0
 module load mpi4py/3.1.6
 module load arrow
-module load opencv/4.10.0
-module load scipy-stack/2024a
-module load imkl/2025.2.0
-module load rust
-module load cmake
-module load intel
+
+
 
 
 #virtualenv --no-download --clear /home/memole/D2E
 source /home/memole/D2E/bin/activate
-#pip install -r requirements.txt --no-deps
+pip install "sentry-sdk>=2.0.0" "gitpython!=3.1.29,>=1.0.0"
+python -m pip install "pydantic>=2,<3"
+pip install -r requirements.txt --no-deps
 #gsutil -m cp -r gs://dmc_vision_benchmark/dmc_vision_benchmark/locomotion/humanoid_walk/medium /home/memole/scratch/AIME/transition_data/dmc_vb/humanoid_walk
 echo "pretrain VQVAE ....."
 CUDA_VISIBLE_DEVICES=0 python3 -m VRNN.pretrain_vqvae
